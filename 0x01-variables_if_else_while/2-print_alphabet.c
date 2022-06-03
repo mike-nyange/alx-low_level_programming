11537a48204d1972c345f9390631771b58e434d4
mike-nyange <micknyange@gmail.com>
2022-06-03 13:09:18 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char first;
+	for(first = 'a', first <= 'z', first++)
+	{
+		putchar(first);
+	}
+	return (0);
+}
