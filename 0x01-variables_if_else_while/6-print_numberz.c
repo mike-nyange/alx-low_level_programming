726563cd31df1e9774456ac0e37de5c44a9faff0
mike-nyange <micknyange@gmail.com>
2022-06-04 01:17:13 +0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,15 +6,17 @@
  * Return: Always 0 (Success)
  */
 
+
 int main(void)
 {
-	int c;
+	int num;
 
-	for (c = '0'; c <= '9'; c++)
+
+	for (num = '0'; num <= '9'; num++)
 	{
-		putchar(c);
+		putchar(num);
 	}
+	putchar('\n');
 
-	putchar("\n");
 	return (0);
 }
