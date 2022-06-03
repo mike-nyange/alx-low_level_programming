d4a6db387e42f3e645ebe7251c6e34e3492ea015
mike-nyange <micknyange@gmail.com>
2022-06-03 14:29:58 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char first;
+	for (first = 'a'; first <= 'z'; first++)
+	{
+		if (first != 'e' && first != 'q')
+		{
+			putchar(first);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
