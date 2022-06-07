95b627ffc013c4fdf5e288c040b3ea47fa6a69ca
mike-nyange <micknyange@gmail.com>
2022-06-07 23:13:21 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,17 +14,27 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			_putchar(i * 0);
-			_putchar(i * 1);
-			_putchar(i * 2);
-			_putchar(i * 3);
-			_putchar(i * 4);
-			_putchar(i * 5);
-			_putchar(i * 6);
-			_putchar(i * 7);
-			_putchar(i * 8);
-			_putchar(i * 9);
-			_putchar('\n');
+			multiplied_value = x * y;
+			if (x == 0)
+			{
+				_putchar(multiplied_value + '0');
+			}
+			else if (multiplied_value >= 10)
+			{
+				_putchar(' ');
+				_putchar(multiplied_value / 10 + '0');
+				_putchar(multiplied_value % 10 + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(multiplied_value + '0');
+			}
+			if (x != 9)
+			{
+				_putchar(',');
+			}			
 		}
 	}
 }
