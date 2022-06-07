08a7715022d4d2582e7ff866ecc03582eb89e55e
mike-nyange <micknyange@gmail.com>
2022-06-07 12:26:06 -0700

    8-24hours.c

---
+++ 0x02-functions_nested_loops/8-24hours.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minute of the day
+ *
+ * Return: every minute of the day
+ */
+
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 24; i++)
+	{
+		for (i=j; j<= 60; j++)
+		{
+			_putchar();
+		}
+	}
+}
