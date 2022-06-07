3162b6800ea7b0019b73061740a7c9d305f3f88c
mike-nyange <micknyange@gmail.com>
2022-06-07 22:29:58 +0300

    Update 8-24hours.c

--- 0x02-functions_nested_loops/8-24hours.c
+++ 0x02-functions_nested_loops/8-24hours.c
@@ -12,9 +12,14 @@ void jack_bauer(void)
 
 	for (i = 0; i <= 24; i++)
 	{
-		for (i=j; j<= 60; j++)
+		for (j=0; j<= 60; j++)
 		{
-			_putchar();
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
