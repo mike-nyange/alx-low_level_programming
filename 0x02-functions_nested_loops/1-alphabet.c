95a44e436c58dfbe17da02c44b60d78f6581d909
mike-nyange <micknyange@gmail.com>
2022-06-07 16:21:41 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * main - print the alphabet
+ * print_alphabet - print the alphabet
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
+
 void print_alphabet(void)
 {
 	int i;
