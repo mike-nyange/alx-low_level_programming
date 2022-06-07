ca4014a30dd9375288ba96ea2d22da52e4014fc3
mike-nyange <micknyange@gmail.com>
2022-06-07 13:01:41 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: 9 times table
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar(i * 0);
+			_putchar(i * 1);
+			_putchar(i * 2);
+			_putchar(i * 3);
+			_putchar(i * 4);
+			_putchar(i * 5);
+			_putchar(i * 6);
+			_putchar(i * 7);
+			_putchar(i * 8);
+			_putchar(i * 9);
+			_puthcar('\n');
+		}
+	}
+}
