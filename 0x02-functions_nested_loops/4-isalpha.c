0ff1c717d37510f9ebd77bc0739e4a23f0309243
mike-nyange <micknyange@gmail.com>
2022-06-07 20:51:51 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,18 +4,15 @@
  * main - check the code
  * Return: Always 0.
  */
-
-int main(void)
+int _isalpha(int c)
 {
-	int _isalpha(int c)
+	if (((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)))
 	{
-		if (((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)))
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
+		return (1);
 	}
+	else
+	{
+		return (0);
+	}
+}
 }
