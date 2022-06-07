85f790102f37e510e2e93164fffe24d6af50c064
mike-nyange <micknyange@gmail.com>
2022-06-07 06:11:45 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print the alphabet
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	return (0);
+}
