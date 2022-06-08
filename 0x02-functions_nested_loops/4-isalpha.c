9acfc8de760dca7a93b6fdcd0752979cd9d78cd8
mike-nyange <micknyange@gmail.com>
2022-06-08 07:17:14 -0700

    change

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * _isalpha - check the code for Holberton School students.
+ * _isalpha - checks for alphabetic characters.
  *
  * @c: is c an integer argument
  *
- * Return: 1 if lowercase or uppercase, 0 otherwise
+ * Return: 1 if c is a letter lowercase or uppercase, 0 otherwise
  */
 
 int _isalpha(int c)
 {
-
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
