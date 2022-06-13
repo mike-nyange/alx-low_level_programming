03b8aac70a8e21ea5434dbda8b118629000c56e5
mike-nyange <micknyange@gmail.com>
2022-06-13 20:12:08 +0000

    6-puts.c update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,16 +6,15 @@
  */
 void puts2(char *str)
 {
-	int i, j=0;
+	int i = 0;
 
 	while (str[i] != '\0')
 	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 		i++;
-	}
-	while (j < i)
-	{
-		_putchar(str[j]);
-		j += 2;
 	}
 	_putchar('\n');
 }
