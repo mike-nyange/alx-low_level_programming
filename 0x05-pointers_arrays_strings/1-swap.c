437c0f0e06247512aa6574b3ab78294cc4476288
mike-nyange <micknyange@gmail.com>
2022-06-13 05:08:34 -0700

    1-swap.c update

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,6 +4,8 @@
  * swap_int: a fuction that swaps the value of two integers.
  * @a: to be swapped with b
  * @b: to be swapped with a
+ *
+ * Description: swap_int swaps the value of *a and *b
  */
 
 void swap_int(int *a, int *b)
