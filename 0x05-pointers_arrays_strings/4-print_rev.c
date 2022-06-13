826c3e57651736a06d5511fd5cf36d0f51bb85ca
mike-nyange <micknyange@gmail.com>
2022-06-13 09:03:43 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - prints reversed string.
+ *@s: pointer to the string that we are printing in reverse
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while(i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
