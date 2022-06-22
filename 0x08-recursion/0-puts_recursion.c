6c813d501f835e2debcc861a2d3c841ce3d34299
mike-nyange <micknyange@gmail.com>
2022-06-23 01:01:46 +0300

    update

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "mainn.h"
+
+/**
+ * _puts_recursion - a function that print a string.
+ * @s: pointer to char
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
