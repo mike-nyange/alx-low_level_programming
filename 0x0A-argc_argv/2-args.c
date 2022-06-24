0b058638ece2a1d85c30b9467204ec9ba8faf17b
mike-nyange <micknyange@gmail.com>
2022-06-24 18:19:13 +0000

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,14 +8,13 @@
  * Return: Always 0 (success)
  */
 
-
 int main(int argc, char *argv[])
 {
-    int i;
+	int i;
 
-    for (i = 0; i < argc; i++)
-    {
-        printf("%s\n", argv[i]);
-    }
-    return (0);
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
 }
