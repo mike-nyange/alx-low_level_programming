12637b0e412ddc687cfd7372baf983ef7253eba0
mike-nyange <micknyange@gmail.com>
2022-06-24 21:41:54 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,20 +12,17 @@
 
 int main(int argc, char *argv[])
 {
-    int i, result;
-    
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    else
-    {
-        for (i = 1; i < argc; i++)
-        {
-           int x = strtol(argv[i], NULL, 10);
-           result *= x;
-            printf("%d\n", result);
-        }
-    }
+	int i, val = 1;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		val *= atoi(argv[i]);
+	}
+	printf("%d\n", val);
+	return (0);
 }
