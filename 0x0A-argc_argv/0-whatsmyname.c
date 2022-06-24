e4d2b7303bfb691d21ad658e9d0f18c7db748363
mike-nyange <micknyange@gmail.com>
2022-06-24 17:36:57 +0000

    0-whatsmyname update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,14 @@
-#include <stdio.h>
+B#include <stdio.h>
 
 /**
 * main - print name of the file
+* @argc: argv count
+* @argv: char string
 * Return - Always 0 (Success)
 */
 
 int main (int argc, char* argv[])
 {
 	printf("%s", argv[argc - 1]);
+	return (0);
 }
