c07882514b4ef65515df5fcdfa837092068543ca
mike-nyange <micknyange@gmail.com>
2022-06-24 21:35:44 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -24,8 +24,8 @@ int main(int argc, char *argv[])
         for (i = 1; i < argc; i++)
         {
            int x = strtol(argv[i], NULL, 10);
-           result *= x
+           result *= x;
+            printf("%d\n", result);
         }
-        printf("%d\n", result)
     }
 }
