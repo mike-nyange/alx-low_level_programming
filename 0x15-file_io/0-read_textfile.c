0e0e9dd5f3491afda84d3426618acb5f361bb797
mike-nyange <micknyange@gmail.com>
2022-07-25 23:08:32 +0300

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,24 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include "holberton.h"
 
 /**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
+ * read_textfile - that reads a text file and prints
+ * @filename: variable pointer
+ * @letters: size letters
+ * Description: Write a function that reads a text file and prints it
+ * to the POSIX standard output.
+ * Return: the actual number of letters it could read and print, 0 otherwise
  */
-int main(int ac, char **av)
-{
-    ssize_t n;
 
-    if (ac != 2)
-    {
-        dprintf(2, "Usage: %s filename\n", av[0]);
-        exit(1);
-    }
-    n = read_textfile(av[1], 114);
-    printf("\n(printed chars: %li)\n", n);
-    n = read_textfile(av[1], 1024);
-    printf("\n(printed chars: %li)\n", n);
-    return (0);
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t file, let, w;
+	char *text;
+
+	text = malloc(letters);
+	if (text == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+
+	if (file == -1)
+	{
+		free(text);
+		return (0);
+	}
+
+	let = read(file, text, letters);
+
+	w = write(STDOUT_FILENO, text, let);
+
+	close(file);
+
+	return (w);
 }
