10b33cc27998a13880e5f2eb5ff74e98e00ec56f
mike-nyange <micknyange@gmail.com>
2022-09-02 00:24:36 +0300

    update

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,11 @@
 #ifndef LISTS_H
 #define LISTS_H
+
+/*
+ * File: lists.h
+ * Auth: Bamidele Adefolaju
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,6 +15,8 @@
  * @prev: points to the previous node
  * @next: points to the next node
  *
+ * Description: doubly linked list node structure
+ * for Holberton project
  */
 typedef struct dlistint_s
 {
@@ -18,6 +26,13 @@ typedef struct dlistint_s
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
-
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+int sum_dlistint(dlistint_t *head);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 #endif /* LISTS_H */
