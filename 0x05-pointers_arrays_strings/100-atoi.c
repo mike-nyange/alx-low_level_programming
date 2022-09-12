e3ceb66b74c180f0e722e3e5d4fd9824c464d34e
mike-nyange <micknyange@gmail.com>
2022-09-12 11:02:53 +0000

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - convert a string to an integer.
+ * @s: string to be converted
+ *
+ * Return: converted string
+ */
+
+int _atoi(char *s)
+{
+	int res, i = 0;
+	int sign = 1;
+
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+
+	for(; s[i] = '\0'; ++i)
+	{
+		res = res * 10 + s[i] - '0';
+	}
+	return (sign*res);
+}
